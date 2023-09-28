362fded9f459c63e376c2201f1c0f05d1dc4e886
VickyKolomiiets <viktoriakolomiets24@gmail.com>
2023-09-28 22:08:34 +0200

    initial commit

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 export const state = {
     recipe: {}
@@ -6,14 +8,10 @@ export const state = {
 
 export const loadRecipe = async function(id) {
 try {
-    const res = await fetch(
-        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-      const data = await res.json();
+    const data = await getJSON(`${API_URL}/${id}`);
   
-      if(!res.ok) throw new Error(`${data.message} (${res.status})`)
-  
-      const {recipe} = data.data;
-      state.recipe = {
+    const {recipe} = data.data;
+    state.recipe = {
         id: recipe.id,
         title: recipe.title,
         publisher: recipe.publisher,
@@ -25,6 +23,7 @@ try {
       }
       console.log(state.recipe)
     } catch (err) {
-        alert(err)
+        // Temporary error handling
+        console.error(`${err} 💥💥💥💥💥💥💥`);
     }
 } 
