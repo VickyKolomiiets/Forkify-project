7d0cb5b84e1632f66d92eb829c73dee736ccd7d5
VickyKolomiiets <viktoriakolomiets24@gmail.com>
2023-10-14 17:06:15 +0200

    initial commit

--- src/js/model.js
+++ src/js/model.js
@@ -9,8 +9,9 @@ export const state = {
       results: [],
       page: 1,
       resultsPerPage: RES_PER_PAGE,
-    }
-}
+    },
+    bookmarks: [],
+};
 
 export const loadRecipe = async function(id) {
 try {
@@ -26,7 +27,13 @@ try {
         servings: recipe.servings,
         cookingTime: recipe.cooking_time,
         ingredients: recipe.ingredients
-      }
+      };
+
+      if(state.bookmarks.some(bookmark => bookmark.id === id))
+         state.recipe.bookmarked = true;
+        else 
+        state.recipe.bookmarked = false;
+
       console.log(state.recipe)
     } catch (err) {
         // Temporary error handling
@@ -50,6 +57,7 @@ export const loadSearchResults = async function(query) {
         image: rec.image_url, 
         };
     });
+    state.search.page = 1;
    } catch (err) {
     console.error(`${err} 💥💥💥`);
     throw err;
@@ -63,7 +71,7 @@ export const getSearchResultsPage = function(page = state.search.page) {
     const end = page * state.search.resultsPerPage; // 9
 
     return state.search.results.slice(start, end);
-}
+};
 
 export const updateServings = function(newServings) {
     state.recipe.ingredients.forEach(ing => {
@@ -74,3 +82,19 @@ export const updateServings = function(newServings) {
     state.recipe.servings = newServings;
 };
 
+export const addBookmark = function(recipe) {
+    // Add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmarked
+    if(recipe.id === state.recipe.id) state.recipe.bookmarked = true; 
+};
+
+export const deleteBookmark = function(id) {
+    // Delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    // Mark current recipe as NOT bookmarked
+    if(id === state.recipe.id) state.recipe.bookmarked = false; 
+}
