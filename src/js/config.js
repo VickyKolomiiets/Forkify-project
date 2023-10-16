7f891af7b4ca9b4038f0c2f5c3fa4ba5c48a567b
VickyKolomiiets <viktoriakolomiets24@gmail.com>
2023-10-16 21:39:16 +0200

    working on uploading recipe feature

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'afa25249-b231-46e6-86f1-fbea33bd33f3';
+export const MODAL_CLOSE_SEC = 2.5
